347589a01c16f6eb5e863e2f5b67a151a2bd5d3e
tnakaicode <tnakaicode@gmail.com>
2021-01-07 08:59:25 +0900

    example run

--- Examples/CavityTuning/Run_TC.bat
+++ Examples/CavityTuning/Run_TC.bat
@@ -9,12 +9,12 @@ Rem  Unauthorized commercial use is prohibited.
 Rem  Remove "Rem" from the next line to prevent codes from opening display windows.   
 Rem set screenoutput=no
 
-cd "%SFDIR%Examples\CavityTuning\CCDTL"
+cd "CavityTuning\CCDTL"
 START /W  " "  "%SFDIR%cdtfish" 1dtfc
 START /W  " "  "%SFDIR%cdtfish" 2dthc
 
 
-cd "%SFDIR%Examples\CavityTuning\CCL"
+cd "CavityTuning\CCL"
 START /W  " "  "%SFDIR%cclfish" 805ccl
 START /W  " "  "%SFDIR%cclfish" 805er
 START /W  " "  "%SFDIR%cclfish" 805wr
@@ -22,13 +22,13 @@ START /W  " "  "%SFDIR%cclfish" 3ghz
 START /W  " "  "%SFDIR%cclfish" 425ew
 
 
-cd "%SFDIR%Examples\CavityTuning\DTL"
+cd "CavityTuning\DTL"
 START /W  " "  "%SFDIR%dtlfish"  dtltest
 START /W  " "  "%SFDIR%dtlfish"  rgfish
 START /W  " "  "%SFDIR%sfotable" rgdtl.sft
 
 
-cd "%SFDIR%Examples\CavityTuning\EllipticalCavity"
+cd "CavityTuning\EllipticalCavity"
 START /W  " "  "%SFDIR%ellfish"  82b
 START /W  " "  "%SFDIR%ellfish"  82be
 START /W  " "  "%SFDIR%ellcav"   82be00
@@ -37,18 +37,18 @@ START /W  " "  "%SFDIR%sfo"      8c82be1.seg
 START /W  " "  "%SFDIR%segfield" segfld.sgf
 
 
-cd "%SFDIR%Examples\CavityTuning\MulticellDTL"
+cd "CavityTuning\MulticellDTL"
 START /W  " "  "%SFDIR%mdtfish" mdttest
 
 
-cd "%SFDIR%Examples\CavityTuning\RFQ"
+cd "CavityTuning\RFQ"
 START /W  " "  "%SFDIR%rfqfish" rfqtest
 START /W  " "  "%SFDIR%rfqfish" lansce
 
 
-cd "%SFDIR%Examples\CavityTuning\SideCouplingCell"
+cd "CavityTuning\SideCouplingCell"
 START /W  " "  "%SFDIR%sccfish" 805cc
 START /W  " "  "%SFDIR%sccfish" lf
 
 
-cd "%SFDIR%Examples\CavityTuning"
+cd "CavityTuning"
