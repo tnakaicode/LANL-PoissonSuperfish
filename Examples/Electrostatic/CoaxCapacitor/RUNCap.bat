df4e4bb092d2dc421f5dd679638429d917080fec
tnakaicode <tnakaicode@gmail.com>
2021-01-07 11:44:08 +0900

    RadioFrequency example run

--- Examples/Electrostatic/CoaxCapacitor/RUNCap.bat
+++ Examples/Electrostatic/CoaxCapacitor/RUNCap.bat
@@ -3,11 +3,11 @@ START /W  " "  "%SFDIR%poisson"   caprz
 copy outaut.txt outaut1.txt
 copy outpoi.txt outpoi1.txt
 copy outpoi.tbl outpoi1.tbl
-if (%1)==(p) START /W  " "  "%SFDIR%WSFplot" caprz.t35  3
+START /W  " "  "%SFDIR%WSFplot" caprz.t35  3
 
 START /W  " "  "%SFDIR%automesh"  capxy
 START /W  " "  "%SFDIR%poisson"   capxy
 copy outaut.txt outaut2.txt
 copy outpoi.txt outpoi2.txt
 copy outpoi.tbl outpoi2.tbl
-if (%1)==(p) START /W  " "  "%SFDIR%WSFplot" capxy.t35  3
+START /W  " "  "%SFDIR%WSFplot" capxy.t35  3
