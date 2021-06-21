22ec9bdab956277147fdb76c5a5b94919501fe87
tnakaicode <tnakaicode@gmail.com>
2021-06-21 18:01:24 +0900

    RFQ LANSCE

---
+++ Examples/CavityTuning/RFQ/LANSCE.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+import shutil
+import subprocess
+from optparse import OptionParser
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./")
+    parser.add_option("--name", dest="name", default="LANSCE")
+    parser.add_option("--pxyz", dest="pxyz",
+                      default=[0.0, 0.0, 0.0], type="float", nargs=3)
+    opt, argc = parser.parse_args(argvs)
+    print(opt, argc)
+
+    print(os.environ["SFDIR"])
+
+    name = opt.name
+    sfdir = os.environ["SFDIR"]
+    #shutil.copyfile("SF.INI", sfdir + "SF.INI")
+    subprocess.call("{0:}{1:} {2:} 3".format(sfdir, "RFQFISH", name + ".rfq"))
+    #subprocess.call("{0:}{1:} {2:}".format(sfdir, "AUTOFISH", name + ".t35"))
+    subprocess.call("{0:}{1:} {2:} 4".format(sfdir, "WSFPLOT", "LA1.t35"))
+    subprocess.call("{0:}{1:} {2:} 4".format(sfdir, "WSFPLOT", "LA2.t35"))
+    subprocess.call("{0:}{1:} {2:} 4".format(sfdir, "WSFPLOT", "LA3.t35"))
+    subprocess.call("{0:}{1:} {2:} 4".format(sfdir, "WSFPLOT", "LA4.t35"))
+    #subprocess.Popen("{0:}{1:} {2:}".format(sfdir, "SF7", name + ".t35"))
+    #shutil.copyfile("../../../SF.INI", sfdir + "SF.INI")
+    # pgui.press("c")
+
+    #
+    # %SFDIR%autofish file
+    # %SFDIR%automesh file
+    # %SFDIR%automesh file
+    # %SFDIR%Tablplot file.tbl sec
+    # %SFDIR%Tablplot file.tbl sec
+    # %SFDIR%WSFplot  file.t35 sec
+    #
+
+    # %SFDIR%AUTOFISH.EXE
+    # %SFDIR%AUTOMESH.EXE
+    # %SFDIR%beta.exe
+    # %SFDIR%CCLCELLS.EXE
+    # %SFDIR%CCLFISH.EXE
+    # %SFDIR%CDTFISH.EXE
+    # %SFDIR%CFISH.EXE
+    # %SFDIR%CONVERTF.EXE
+    # %SFDIR%DTLCELLS.EXE
+    # %SFDIR%DTLFISH.EXE
+    # %SFDIR%ELLCAV.EXE
+    # %SFDIR%ELLFISH.EXE
+    # %SFDIR%FISH.EXE
+    # %SFDIR%FORCE.EXE
+    # %SFDIR%FSCALE.EXE
+    # %SFDIR%KILPAT.EXE
+    # %SFDIR%LIST35.EXE
+    # %SFDIR%MDTCELLS.EXE
+    # %SFDIR%MDTFISH.EXE
+    # %SFDIR%MK_SFINI.EXE
+    # %SFDIR%PANDIRA.EXE
+    # %SFDIR%POISSON.EXE
+    # %SFDIR%Quikplot.exe
+    # %SFDIR%RFQFISH.EXE
+    # %SFDIR%SCCFISH.EXE
+    # %SFDIR%SEGFIELD.EXE
+    # %SFDIR%SF7.EXE
+    # %SFDIR%SF8.EXE
+    # %SFDIR%SFO.EXE
+    # %SFDIR%SFOTABLE.EXE
+    # %SFDIR%Tablplot.exe
+    # %SFDIR%wsfcolor.exe
+    # %SFDIR%WSFPLOT.EXE
+    #
---
+++ Examples/CavityTuning/RFQ/NOTE.md
@@ -0,0 +1,24 @@
+---
+title: Cavity
+---
+
+## LANSCE
+
+; Start codes for RFQFISH (Nv parameter on DATA lines):
+; 1 No tuning
+; 2 No tuning
+; 3 Adjust vane-tip radius rho
+; 4 Adjust vane-shoulder half width Ws
+; 5 Adjust vane-shoulder length Ls
+; 6 Adjust vane-base half width Wb
+; 7 Adjust vane-base height H
+; 8 Adjust vane half width W
+
+|     | Nv=1              | Nv=3              |
+| --- | ----------------- | ----------------- |
+| LA1 | ![pic](LA101.png) | ![pic](LA102.png) |
+| LA2 | ![pic](LA201.png) | ![pic](LA202.png) |
+| LA3 | ![pic](LA301.png) | ![pic](LA302.png) |
+| LA4 | ![pic](LA401.png) | ![pic](LA402.png) |
+
+## RFQ
